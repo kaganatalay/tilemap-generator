46e28a90222a90a7492876dbce5ee38aed42ee11
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-24 15:54:34 +0300

    camera movement

--- sketch.js
+++ sketch.js
@@ -6,12 +6,11 @@ let tilemap = {
   ],
 }
 
-let x = 0;
-let y = 0;
+let offset;
 
 function setup() {
   createCanvas(800, 800);
-
+  offset = createVector(0, 0);
   tilemap.size = createVector(160, 160);
 }
 
@@ -19,13 +18,38 @@ function draw() {
   background(236, 232, 225);
 
   push();
-
-  pop();
-
+  translate(width/2 - offset.x, height/2 - offset.y);
   for(let y = 0; y < tilemap.src.length; y++) {
     for(let x = 0; x < tilemap.src[y].length; x++) {
       rect(x * tilemap.size.x, y * tilemap.size.y, tilemap.size.x, tilemap.size.y);
     }
   }
 
+  if(mouseIsPressed) {
+    let diff = createVector();
+    diff.x = pmouseX - mouseX;
+    diff.y = pmouseY - mouseY;
+
+    offset.add(diff);
+  }
+  pop();
+
+  
+
+  if(keyIsDown(65)) {
+    offset.x--;
+  }
+
+  if(keyIsDown(68)) {
+    offset.x++;
+  }
+
+  if(keyIsDown(87)) {
+    offset.y--;
+  }
+
+  if(keyIsDown(83)) {
+    offset.y++;
+  }
+ 
 }
