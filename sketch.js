eaceb7541bb23111cc84363d325b4ab56a2926c6
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-24 15:48:41 +0300

    generating the grid

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
+let tilemap = {
+  src: [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3]
+  ],
+}
+
+let x = 0;
+let y = 0;
+
 function setup() {
   createCanvas(800, 800);
+
+  tilemap.size = createVector(160, 160);
 }
 
 function draw() {
   background(236, 232, 225);
 
-  
+  push();
+
+  pop();
+
+  for(let y = 0; y < tilemap.src.length; y++) {
+    for(let x = 0; x < tilemap.src[y].length; x++) {
+      rect(x * tilemap.size.x, y * tilemap.size.y, tilemap.size.x, tilemap.size.y);
+    }
+  }
+
 }
