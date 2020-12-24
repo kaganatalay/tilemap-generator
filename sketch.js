c9018c79103dc047ffb9117176ae83cc5576fbd1
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-24 20:23:24 +0300

    broken hover system

--- sketch.js
+++ sketch.js
@@ -2,17 +2,19 @@ let tilemap = {
   src: [],
 };
 
-let columns = 15;
-let rows = 15;
+let columns = 5;
+let rows = 5;
 
 let offset;
 let resolution = 1;
 let canvas;
+let mouse;
 
 function setup() {
   createCanvas(800, 800);
   offset = createVector(0, 0);
   tilemap.size = createVector(160, 160);
+  mouse = createVector();
   canvas = createVector(width, height);
   
   for(let i = 0; i < rows; i++) {
@@ -28,13 +30,24 @@ function setup() {
 function draw() {
   background(236, 232, 225);
 
+  mouse.x = mouseX + offset.x - width/2;
+  mouse.y = mouseY + offset.y - height/2;
+
   push();
   scale(resolution);
   translate(canvas.x/2 - offset.x, canvas.y/2 - offset.y);
 
   for(let y = 0; y < tilemap.src.length; y++) {
     for(let x = 0; x < tilemap.src[y].length; x++) {
-      rect(x * tilemap.size.x, y * tilemap.size.y, tilemap.size.x, tilemap.size.y);
+      let position = createVector();
+      position.x = x * tilemap.size.x;
+      position.y = y * tilemap.size.y;
+      if(hovering(position.x, position.y, tilemap.size.x, tilemap.size.y)) {
+        fill(255, 150, 0);
+      } else {
+        fill(255);
+      }
+      rect(position.x, position.y, tilemap.size.x, tilemap.size.y);
     }
   }
 
@@ -65,6 +78,10 @@ function draw() {
     offset.y++;
   }
  
+}
+
+function hovering(x, y, w, h) {
+  return mouse.x >= x && mouse.x <= x + w && mouse.y >= y && mouse.y <= y + h;
 }
 
 function mouseWheel(event) {
